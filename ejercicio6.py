be2750c37bb3a07afa89b2b9fec7ff562970d0df
Mario <mariodiaz1375@gmail.com>
2025-04-08 20:24:57 -0300

    finalizado el ejercicio 6

--- ejercicio6.py
+++ ejercicio6.py
@@ -2,45 +2,6 @@ from crear_matriz import crear_matriz, crear_matriz_cuadrada
 
 from ejercicio1 import ingresar_numero, ingresar_numero_entero
 
-# def desplegar_menu():
-#     try:
-#         while True:
-#             print('1. Calcular potencia')
-#             print('2. Obtener los digitos de un numero')
-#             print('3. Comprobar si un numero es capicua')
-#             print('4. Salir')
-#             opcion = input('Ingrese una opcion: ')
-
-#             if opcion == '1':
-            
-#                 print('\nIngrese la base')
-#                 base = ingresar_numero()
-#                 print('Ingrese el exponente')
-#                 exponente = ingresar_numero_entero()
-#                 resultado = calcular_potencia(base, exponente)
-#                 print(f'El resultado de la potencia es: {resultado}\n')
-
-#             elif opcion == '2':
-                
-#                 print('\nContar digitos')
-#                 numero = ingresar_numero_entero()
-#                 digitos = contar_digitos(numero)
-#                 print(f'El numero {numero} tiene {digitos} digitos\n')
-
-#             elif opcion == '3':
-                
-#                 print('\nNumero capicua')
-#                 numero = ingresar_numero_entero()
-#                 capicua = det_capicua(numero)
-#                 print(f'El numero {numero} es capicua? {capicua}\n')
-
-#             elif opcion == '4':
-#                 break
-
-#             else:
-#                 print('Ingrese una opcion valida')
-#     except Exception as e:
-#         print(f'Ocurrio un error: {e}')
 
 def comprobar_producto(a, b):
     if len(a[0]) == len(b):
@@ -91,7 +52,7 @@ if __name__ == '__main__':
     # for fila in resultado:
     #     print(fila)
 
-    print('resultado: ')
+    print('\nResultado de la multiplicacion de matrices: ')
     matriz_p = multiplicar_matrices(matriz1, matriz2)
     for fila in matriz_p:
         print(fila)
