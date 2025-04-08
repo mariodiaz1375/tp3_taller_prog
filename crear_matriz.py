84b196292ac9b73135a119d461b5837af6a8b9ca
Mario <mariodiaz1375@gmail.com>
2025-04-08 17:25:41 -0300

    se corrigieron las funciones de matrices

--- crear_matriz.py
+++ crear_matriz.py
@@ -1,15 +1,21 @@
-from crear_vector import crear_vector_entero
+from crear_vector import crear_vector_entero, crear_vector_natural
 
 def crear_matriz_cuadrada(filas):
     matriz = []
     for i in range(filas):
-        matriz.append(crear_vector_entero(filas))
+        matriz.append(crear_vector_natural(filas))
     return matriz
 
 def crear_matriz(filas, columnas):
     matriz = []
     for i in range(filas):
         matriz.append(crear_vector_entero(columnas))
+    return matriz
+
+def crear_matriz_natural(filas, columnas):
+    matriz = []
+    for i in range(filas):
+        matriz.append(crear_vector_natural(columnas))
     return matriz
 
     
