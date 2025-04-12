83bc58b48589a3d8175bfd701b1f664e7a5eb616
mario <mariodiaz1375@gmail.com>
2025-04-12 01:36:28 -0300

    terminado el ejercicio 10

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,23 +1,17 @@
 import random as ran
 
-rodillos = [
-    ['X', '0', '7', 'X', '0', '7', 'X', '0', '7'],
-    ['0', '7', 'X', '0', '7', 'X', '0', '7', 'X'],
-    ['7', 'X', '0', '7', 'X', '0', '7', 'X', '0']
-]
 
-def tirar_numero():
-    numero = ran.randint(0,9)
-    return numero
+def crear_numeros(cantidad):
+    numeros = []
+    for i in range(cantidad):
+        numeros.append(ran.randint(0,9))
+    return numeros
 
-def mover_rodillo(rodillos, num1, num2, num3):
+def mover_rodillo(rodillos, numeros):
     resultado = []
-    lista_r = []
-    numeros = [num1, num2, num3]
     for i in range(len(rodillos)):
-        rodillos[i] = rotar_fila[lista, numeros[i]]
-        
-    return rodillos
+        resultado.append(rotar_fila(rodillos[i], numeros[i]))
+    return resultado
 
 def rotar_fila(lista, num):
     for i in range(num):
@@ -27,16 +21,44 @@ def rotar_fila(lista, num):
         lista[8] = primero
     return lista
 
+def ganar(rodillos):
+    if rodillos[0][0] == rodillos[1][0] == rodillos[2][0]:
+        return True
+    else:
+        return False
+
 def jugar():
-    while True:
-        print('Presione 1 para tirar el primer numero')
+    rodillos = [
+        ['X', '0', '7', 'X', '0', '7', 'X', '0', '7'],
+        ['0', '7', 'X', '0', '7', 'X', '0', '7', 'X'],
+        ['7', 'X', '0', '7', 'X', '0', '7', 'X', '0']
+    ]
+    try:
+        while True:
+            print('1. Para jugar')
+            print('2. Para salir')
+            opcion = input('Ingrese la opcion: ')
+            if opcion == '1':
+                numeros = crear_numeros(3)
+                resultado = mover_rodillo(rodillos, numeros)
+                print('\nResultado: ')
+                print(f'\n{rodillos[0][0]} - {rodillos[1][0]} - {rodillos[2][0]}\n')
+                if ganar(resultado) and resultado[0][0] == 'X':
+                    print('Ganaste 10 fichas')
+                elif ganar(resultado) and resultado[0][0] == '0':
+                    print('Ganaste 100 fichas')
+                elif ganar(resultado) and resultado[0][0] == '7':
+                    print('Ganaste 1000 fichas')
+                else:
+                    print("Perdiste, vuelve a intentarlo")
+            elif opcion == '2':
+                break
+            else:
+                print('Ingrese una opcion valida')
+    except Exception as e:
+        print(f'Ocurrio un error: {e}')
 
+if __name__ == '__main__':
+    jugar()
 
-# resultado = mover_rodillo(5,0,rodillos)
-# for fila in resultado:
-#     print(fila)
-
-print('por fila')
-lista = rotar_fila(rodillos[0], 5)
-print(lista)
 
